8c48adf233b08b9c5ef929484c864eadff112b6a
KeenGWatanabe <mckeenweb@gmail.com>
2025-03-19 06:23:02 +0800

    default vpc cidr

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #1 vpc
 resource "aws_vpc" "roger_vpc" {
-  cidr_block           = var.vpc_cidr_block
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   tags = {
     Name = "roger_vpc"
